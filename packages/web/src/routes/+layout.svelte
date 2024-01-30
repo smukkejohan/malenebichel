df9b1ae0383387c898a3e9e5bb43b7e6c829dc2e
Johan Bichel <public@johan.cc>
2024-01-30 14:06:19 +0100

    courses and styling

--- packages/web/src/routes/+layout.svelte
+++ packages/web/src/routes/+layout.svelte
@@ -1,214 +1,136 @@
 <script>
-    import "../app.css";
-  </script>
+	import '../app.css';
+</script>
 
 <svelte:head>
-    <title>Malene Bichel, Sanger og proceskonsulent</title>
-    <meta name="description" content="">
-
+	<title>Malene Bichel, Sanger og proceskonsulent</title>
+	<meta name="description" content="" />
 </svelte:head>
 
 <div class="flex flex-col h-full">
+	<header id="siteheader" class="container mx-auto">
+		<hgroup>
+			<h1 class="text-xl"><a href="/">Malene Bichel</a></h1>
+			<h2 class="text-lg">Sanger og proceskonsulent</h2>
+		</hgroup>
+	</header>
 
-    <header id="siteheader" class="container mx-auto">
-        <hgroup>
-            <h1 class="text-xl"><a href="/">Malene Bichel</a></h1>
-            <h2 class="text-lg">Sanger og proceskonsulent</h2>
-        </hgroup>
-    </header> 
+	<main class="container mx-auto flex gap-4 flex-grow">
+		<section id="content" class="flex-grow prose">
+			<slot />
+		</section>
 
-    <main class="container mx-auto flex gap-4 flex-grow">
+		<aside id="sidebar" class="w-10">
+			<nav id="main-navigation">
+				<ul>
+					<li class="active"><a href="">Forside</a></li>
+					<li><a href="http://malbichel.blogspot.dk/">Blog</a></li>
+					<li class=""><a href="/recommendations/">Anbefalinger</a></li>
+				</ul>
 
-        <section id="content" class="flex-grow">
-            <slot></slot>
-        </section>
+				<ul>
+					<li class="">
+						<a href="/performance/" title="Performance">Performance</a>
+					</li>
+					<li class="">
+						<a href="/malene/" title="Om Malene">Om Malene</a>
 
-        <aside id="sidebar" class="w-10">
-            <nav id="main-navigation">
-                <ul>
-                    <li class="active"><a href="">Forside</a></li>
-                    <li><a href="http://malbichel.blogspot.dk/">Blog</a></li>
-                    <li class=""><a href="/recommendations/">Anbefalinger</a></li>
-                </ul>
-                
-        
-            <ul><li class="">
-        
-    
-        <a href="/performance/" title="Performance">Performance</a>
-        
-            </li><li class="">
-        
-    
-        <a href="/malene/" title="Om Malene">Om Malene</a>
-    
-        
-        
-    
-        
-            <ul><li class="">
-        
-    
-        <a href="/blog/" title="Blog">Blog</a>
-    
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/referencer/" title="Referencer">Referencer</a>
-    
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/inspiration/" title="Inspiration">Inspiration</a>
-    
-        
-            </li></ul>
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/vision/" title="Vision">Vision</a>
-    
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/jordforbindelse-sensualitet/" title=" Stemmeundervisning"> Stemmeundervisning</a>
-    
-        
-        
-    
-        
-            <ul><li class="">
-        
-    
-        <a href="/sang-sangere-performere/" title="Sang for sangere og performere">Sang for sangere og performere</a>
-    
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/stemme-polaritet/" title="Stemme og polaritet">Stemme og polaritet</a>
-    
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/stemme-krop-praestation/" title="Stemme, krop og præstation">Stemme, krop og præstation</a>
-    
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/workshops-for-etablerede-kor/" title="Workshops for etablerede kor ">Workshops for etablerede kor </a>
-    
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/mindfulness-stemmebrug/" title="Mindfulness i dit stemmebrug">Mindfulness i dit stemmebrug</a>
-    
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/stemme-naervaer-kreativitet/" title="Stemme nærvær og kreativitet">Stemme nærvær og kreativitet</a>
-    
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/kreativitet-og-kraft/" title="Individuel undervisning - og små udviklingsgrupper ">Individuel undervisning - og små udviklingsgrupper </a>
-    
-        
-            </li></ul>
-        
-        
-    
-        
-            </li><li class="">
-        
-    
-        <a href="/for-virksomheder/" title="For erhverv ">For erhverv </a>
-    
-        
-            </li></ul>
-        
-        
-    
-            </nav>
-    
-            
-            <section id="facebook">
-            <iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fmalenebichel.dk&amp;width=240&amp;colorscheme=light&amp;show_faces=true&amp;stream=false&amp;header=false&amp;height=192" scrolling="no" frameborder="0" style="border:none; overflow:hidden;
-                width:240px; height:192px;" allowTransparency="true">
-            </iframe>
-        </section>
-    
-            <br/>
-            <p>Kontakt: email <em>malbichel at gmail.com</em> eller telefon <em>52231910</em></p>
-    
-        </aside>    
+						<ul>
+							<li class="">
+								<a href="/blog/" title="Blog">Blog</a>
+							</li>
+							<li class="">
+								<a href="/referencer/" title="Referencer">Referencer</a>
+							</li>
+							<li class="">
+								<a href="/inspiration/" title="Inspiration">Inspiration</a>
+							</li>
+						</ul>
+					</li>
+					<li class="">
+						<a href="/vision/" title="Vision">Vision</a>
+					</li>
+					<li class="">
+						<a href="/stemmeundervisning/" title=" Stemmeundervisning"> Stemmeundervisning</a>
+						<ul>
+							<li class="">
+								<a href="/sang-sangere-performere/" title="Sang for sangere og performere"
+									>Sang for sangere og performere</a
+								>
+							</li>
+							<li class="">
+								<a href="/stemme-polaritet/" title="Stemme og polaritet">Stemme og polaritet</a>
+							</li>
+							<li class="">
+								<a href="/stemme-krop-praestation/" title="Stemme, krop og præstation"
+									>Stemme, krop og præstation</a
+								>
+							</li>
+							<li class="">
+								<a href="/workshops-for-etablerede-kor/" title="Workshops for etablerede kor "
+									>Workshops for etablerede kor
+								</a>
+							</li>
+							<li class="">
+								<a href="/mindfulness-stemmebrug/" title="Mindfulness i dit stemmebrug"
+									>Mindfulness i dit stemmebrug</a
+								>
+							</li>
+							<li class="">
+								<a href="/stemme-naervaer-kreativitet/" title="Stemme nærvær og kreativitet"
+									>Stemme nærvær og kreativitet</a
+								>
+							</li>
+							<li class="">
+								<a
+									href="/kreativitet-og-kraft/"
+									title="Individuel undervisning - og små udviklingsgrupper "
+									>Individuel undervisning - og små udviklingsgrupper
+								</a>
+							</li>
+						</ul>
+					</li>
+					<li class="">
+						<a href="/for-virksomheder/" title="For erhverv ">For erhverv </a>
+					</li>
+				</ul>
+			</nav>
 
-    </main>
+			<section id="facebook">
+				<iframe
+					src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fmalenebichel.dk&amp;width=240&amp;colorscheme=light&amp;show_faces=true&amp;stream=false&amp;header=false&amp;height=192"
+					scrolling="no"
+					frameborder="0"
+					style="border:none; overflow:hidden;
+                width:240px; height:192px;"
+					allowTransparency="true"
+				/>
+			</section>
 
+			<br />
+			<p>Kontakt: email <em>malbichel at gmail.com</em> eller telefon <em>52231910</em></p>
+		</aside>
+	</main>
 
+	<footer id="sitefooter" class="container mx-auto">
+		<!--<p>Hosted on a linode, powered by django and python developed by johan.cc</p>-->
+		<p>Kontakt: email <em>malbichel at gmail.com</em> eller telefon <em>52231910</em></p>
+	</footer>
 
-    <footer id="sitefooter" class="container mx-auto">
-        <!--<p>Hosted on a linode, powered by django and python developed by johan.cc</p>-->
-        <p>Kontakt: email <em>malbichel at gmail.com</em> eller telefon <em>52231910</em></p>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-1946017-3']);
+		_gaq.push(['_trackPageview']);
 
-    </footer>
-
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-1946017-3']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
-
-
+		(function () {
+			var ga = document.createElement('script');
+			ga.type = 'text/javascript';
+			ga.async = true;
+			ga.src =
+				('https:' == document.location.protocol ? 'https://ssl' : 'http://www') +
+				'.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
 </div>
-
-
---
+++ packages/web/src/routes/stemmeundervisning/+page.svelte
@@ -0,0 +1,125 @@
+    
+    <article>
+        <h1 class="text-xl"> Stemmeundervisning</h1>
+        <p>Stemmen er klangen af en vibration formet af følelser, mentale kræfter, muskelarbejde, attitude, energi og meget andet.
+Stemmen kan der ikke være noget galt med. Den er jo lige netop lyden af dig - som du er og som du har det - lige nu.
+Fordi stemmen er det autentiske udtryk for vores følelser, fordomme, begær, indsigter og kropslige formåen, så er det også en sikker indikator for de kropslige og mentale blokeringer, der måtte findes. 
+Utrænede ører registrerer at der er noget galt, eller at man ikke kan li' stemmen, mens trænede ører kan henføre en bestemt klang til en konkret blokering af flow'et i krop eller følelser. </p>
+<p>Arbejdet med stemme og krop med fokus på mindfulness, nærvær og kreativitet kan derfor være indfaldsvinkel til mange forskellige problematikker og udviklingsmål.
+(bl. a. hengivelse/sensualitet, gennemslagskraft, kunstnerisk eller personlig afklaring, - problemer med at synge rent, - med at blive hørt, - med at lytte, fysiske spændinger i tale og sang, stress, udbrændthed, præstationsangst)</p>
+<p>Kontakt mig gerne på malenebi@gmail.com, hvis du ønsker hjælp, inspiration, coaching eller rådgivning som ikke dækkes af de aktuelle workshops. Jeg tilrettelægger så et forløb for en eller flere personer, ud fra det aktuelle behov.</p>
+    </article>
+
+    <article id="sang-sangere-performere" class="mt-4">
+        <h1 class="text-xl">Sang for sangere og performere</h1>
+        <p>For dig, der som udøvende sanger eller performer gerne vil 
+nå dybere i det kunstneriske arbejde 
+arbejde med det autentiske, rørende og bevægende møde med kunsten
+nå det autentiske, rørende og bevægende møde med publikum</p>
+<p>Undervisningen foregår på et mindre hold, 
+og veksler mellem fælles øvelser og individuel vejledning.</p>
+<p>For tiden underviser jeg mest sangere og performere individuelt, -men jeg laver meget gerne denne workshop på opfordring.</p>
+    </article>
+    
+    <article id="stemme-polaritet" class="mt-4">
+        <h1 class="text-xl">Stemme og polaritet</h1>
+        <p>Undervisningen foregår som fælles sang - og stemmeøvelser og som individuel stemmeteknisk vejledning</p>
+<p>Stemmen udtrykker alle aspekter af det vi er. 
+Nogle af menneskets mest fremtrædende aspekter er de kønslige. 
+Både mænd og kvinder har brug for at kunne udtrykke feminine og maskuline aspekter, og ved at arbejde med stemmens udtryksmuligheder vil vi samtidigt arbejde med de - måske ubevidste eller undertrykte - maskuline og feminine værdier. </p>
+<p>En kniv er ikke det modsatte af en gaffel
+Maskulinitet er ikke det modsatte af femininitet
+Der er ikke noget feminint over fravær af mandighed.
+Og der er ikke noget maskulint over fravær af kvindelighed</p>
+<p>Kønsløshed fortrænges af kønsligt forankret væren
+som mørke fortrænges af lys
+</p>
+    </article>
+    
+    <article id="stemme-krop-praestation" class="mt-4">
+        <h1 class="text-xl">Stemme, krop og præstation</h1>
+        <p>Denne workshop er for dig der frygter at blive lammet eller hæmmet af nervøsitet, netop når det er vigtigt for dig at yde dit bedste.</p>
+<p>Få hjælp til at </p>
+<p>vende din fordybelse mod det du vil formidle
+mærke hvordan sceneskræk og anden præstationsangst kan reformeres
+samarbejde med din krop i stedet for at kontrollere den
+opnå tryghed ved din formidling, din krop og din præstation
+Få konkrete råd og hjælp til at håndtere netop din version af et problem som er almindeligt for rigtig mange mennesker.</p>
+<p>Undervisningen foregår på mindre hold i en afslappende og tryg atmosfære og veksler mellem fælles øvelser og individuel vejledning.</p>
+<p>Se i kalenderen hvornår næste workshop afholdes.</p>
+    </article>
+    
+    <article id="workshops-for-etablerede-kor" class="mt-4">
+        <h1 class="text-xl">Workshops for etablerede kor </h1>
+        <p>Har du eller dit kor brug for inspiration og nyt værktøj til at arbejde med </p>
+<ul>
+<li>det individuelle og kollektive stemmebrug?</li>
+<li>klangen i stemmegrupperne?</li>
+<li>nærvær og kreativitet?</li>
+<li>publikumskontakt?</li>
+<li>autonomi i det musikalske udtryk?</li>
+<li>fysisk og psykisk opvarmning og øvning?</li>
+<li>ny musik og anderledes stemmebrug?</li>
+</ul>
+<p>Kan jeg hyres som gæstelærer på koraftener eller weekendworkshops.
+Kontakt mig for et tilbud på malenebi@gmail.com</p>
+    </article>
+    
+
+
+    <article id="mindfulness-stemmebrug" class="mt-4">
+        <h1 class="text-lg">Mindfulness i dit stemmebrug</h1>
+        <p>Undervisningen foregår som fælles sangøvelser og som individuel  sang - og stemmeteknisk vejledning på mindre hold i en tryg og afslappet atmosfære</p>
+<p>Ved at arbejde med stemme og mindfulness vil du</p>
+<ul>
+<li>optræne den årvågne og nysgerrige opmærksomhed og dermed nedtone den kritiske opmærksomhed</li>
+<li>komme i kontakt med dine styrker og blive tryg ved dine sårbarheder</li>
+<li>opøve din evne til  musikalsk hengivelse og sensualitet</li>
+<li>træne klarhed og overblik</li>
+<li>lære at bruge din stemme som indikator på din øvrige tilstand.</li>
+<li>smitte andre med tryghed og tillid med din stemmeklang og lytteevne</li>
+<li>blive mere autentisk i din musikalske kommunikation med andre </li>
+<li>opnå et tantrisk forhold til din stemme og dermed til hele den proces som du er.
+</li>
+</ul>
+    </article>
+    
+    <article id="stemme-naervaer-kreativitet">
+        <h1>Stemme nærvær og kreativitet</h1>
+        <p>Vil du gerne have mere overskud i din måde at bruge stemmen på?</p>
+<p>Undervisningen foregår på mindre hold i en afslappet og tryg atmosfære, og veksler mellem fælles sangøvelser og individuel vejledning i stemmeteknik.</p>
+<p>På dette hold kan du arbejde med
+- at skærpe dit nærvær og din koncentrationskondition 
+- redskaber til at træne musikalsk fordybelse, spontanitet og hengivelse 
+- at vende tilbage til den rene sang og klang, som vi alle er født med og som vi kan bruge autentisk og uden omsvøb. 
+- at åbne den kreative kilde gennem stemmen, når du slipper dine forventninger til hvordan stemmen skal lyde og hvordan du skal virke
+- at blive tryggere ved din stemme, så du kan bruge den alsidigt og levende i tale og sang </p>
+<p>Du arbejder blandt andet med en medbragt sang eller tekst, -evt. i mikrofon
+</p>
+    </article>
+    
+
+    <article id="kreativitet-og-kraft">
+        <h1>Individuel undervisning - og små udviklingsgrupper </h1>
+        <p>Individuel undervisning 
+- i sang, stemmebrug og personlig gennemslagskraft. </p>
+<p>Bliver du rørt af en sang, en tekst eller en anden vigtig sandhed, og lader stemmen udtrykke dette, vil det også røre andre.
+Så simpelt er det. </p>
+<p>Men dit ego kan forstyrre og få dig til at leve dig ind i dig selv i stedet. Det kaldes nervøsitet, generthed, selvhøjtidelighed, hæmninger,
+krukkeri, kedsomhed.
+Kært barn – mange navne
+.</p>
+<p>Lær teknikker til at befri dig selv af denne klemme og få et nærværende og kreativt forhold til din stemme og din kommunikation.
+Og lær at bruge teknikkerne til at hjælpe andre.</p>
+<p>Pris kr 500 pr time
+Introduktionstilbud: De to første timer for kr. 800.</p>
+<p>Kontakt mig for aftale. </p>
+<p>Udviklingsgrupper</p>
+<p>At arbejde med sin krop og stemme sammen med andre er ofte mere virkningsfuldt end at gøre det alene</p>
+<p>Derfor danner jeg gerne små grupper (2-8 personer), hvor der arbejdes med individuel stemmetræning og/eller sangteknik</p>
+<p>Tilmeld dig også gerne sammen med din partner eller med dine børn. Det kan
+være meget smukt at se hinanden folde sig ud stemmemæssigt, og for mange er
+det skønt at arbejde med kommunikation uden at fokusere på at analysere
+hinandens psyke, men at lege med hinandens æstetiske muligheder.</p>
+    </article>
+    
